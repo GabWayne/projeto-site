b2dfa185cdd480eb1c4f1843e17ae214c7334dc4
Gabriel Souza <42553753+GabWayne@users.noreply.github.com>
2020-10-29 12:23:50 -0300

    Correção/Melhoria ex16 e ex17
    Pequenas correções(verificação de erro) e melhorias(uso de emoji e tabelas) nos exercícios 16 e 17

--- javascript/aula14ex/ex016/script.js
+++ javascript/aula14ex/ex016/script.js
@@ -4,18 +4,29 @@ function contar() {
     var fim = document.getElementById('fim')
     var res = document.getElementById('res')
 
-    var inicio = Number(ini.value)
-    var passo = Number(pas.value)
-    var final = Number(fim.value)
+    if (ini.value.length == 0 || pas.value.length == 0 || fim.value.length == 0) {
+        //window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar'
+    } else {
+        var inicio = Number(ini.value)
+        var passo = Number(pas.value)
+        var final = Number(fim.value)
 
-    if (passo <= 0) {
-        window.alert('Impossível contar com passo menor que 1. Considerando passo 1')
-        passo = 1
-    }
+        if (passo <= 0) {
+            window.alert('Impossível contar com passo menor que 1. Considerando passo 1')
+            passo = 1
+        }
 
-    res.innerHTML = ``
-    for (var c = inicio;c <= final;c += passo) {
-        res.innerHTML += `${c} -> `
+        res.innerHTML = ``
+        if (inicio < final) {   //contagem crescente
+            for (var c = inicio;c <= final;c += passo) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {    //contagem decrescente
+            for (var c = inicio;c >= final;c -= passo) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-    res.innerHTML += `Fim!`
 }
--- javascript/aula14ex/ex017/script.js
+++ javascript/aula14ex/ex017/script.js
@@ -1,12 +1,17 @@
 function tabuada() {
     var num = document.getElementById('num')
-    var numero = Number(num.value)
-    var res = document.getElementById('res')
+    var tab = document.getElementById('seltab')
 
-    res.innerHTML = ''
-    for (var c = 1;c <= 10;c++) {
-        var mult = numero * c
-        res.innerHTML += `${numero} x ${c} = ${mult} <br>`
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número')
+    } else {
+        numero = Number(num.value)
+        tab.innerHTML = ''
+        for (var c = 1;c <= 10;c++) {
+            var item = document.createElement('option')
+            item.text = `${numero} x ${c} = ${numero*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
     }
-    res.innerHTML += 'Fim!'
 }
