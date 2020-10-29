52d61ea710d9897402e6b7cc4f79400abd05a096
Gabriel Souza <42553753+GabWayne@users.noreply.github.com>
2020-10-29 10:16:10 -0300

    Primeiro uso de repetições
    Arquivo js com implementação simples de while e do while

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*var passo = 1
+console.log(`Passo número ${passo}`)
+passo++
+console.log(`Passo número ${passo}`)
+passo++
+console.log(`Passo número ${passo}`)
+passo++
+console.log(`Passo número ${passo}`)
+passo++
+console.log(`Passo número ${passo}`)
+*/
+
+/*var passo = 1
+while (passo <= 5) {
+    console.log(`Passo número ${passo}`)
+    passo++
+}*/
+
+var passo = 1
+do {
+    console.log(`Passo número ${passo}`)
+    passo++
+} while (passo <= 5)
