97206b57054e69673fed171b5d7e428e3fadfde5
Gabriel Souza <42553753+GabWayne@users.noreply.github.com>
2020-11-21 15:44:26 -0300

    Finalizando o formulário
    Finalizando o css do formulario da página de contato do site

--- antigo/projeto-glass-html5/_javascript/funcoes.js
+++ antigo/projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,10 @@
 function mudaFoto(foto) {
     document.getElementById('icone').src = foto
 }
+
+function calctotal() {
+    var qvalor = Number(document.getElementById('cquant').value)
+    total = qvalor * 1500
+
+    document.getElementById('ctotal').value = total
+}
